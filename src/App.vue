4b6d845e28f96947c5d35b5e8fc9e96496cb91b5
Wouter Vernaillen <wouter@vernaillen.com>
2022-06-16 08:26:26 +0200

    redirect to links.wouter.net if coming from different hostname

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
 import { RouterView } from "vue-router";
 import IconLogo from "@/components/icons/IconLogo.vue";
 import SocialIcons from "@/components/SocialIcons.vue";
+
+const hostname = location.hostname;
+if (hostname != "links.wouter.net" && hostname != "localhost") {
+  window.location.href = "https://links.wouter.net";
+}
 </script>
 
 <template>
