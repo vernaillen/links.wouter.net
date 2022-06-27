a28a52b812cc29a32d76e514029c68dbe41de4a3
Wouter Vernaillen <wouter@vernaillen.com>
2022-06-27 10:23:56 +0200

    prevent font-awesome icons from flashing on page load

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,12 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 library.add(faInstagram, faLinkedin, faGithub, faTwitter);
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 const routes = [
   {
@@ -25,7 +31,11 @@ export const createApp = ViteSSG(App, { routes }, ({ app, isClient }) => {
   app.component("font-awesome-icon", FontAwesomeIcon);
   if (isClient) {
     const hostname = window.location.hostname;
-    if (hostname != "links.wouter.net" && hostname != "localhost") {
+    if (
+      hostname != "links.wouter.net" &&
+      hostname != "localhost" &&
+      hostname != "127.0.0.1"
+    ) {
       window.location.href = "https://links.wouter.net";
     }
   }
